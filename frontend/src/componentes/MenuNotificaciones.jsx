0e19ee2380a6bcee752a21ae87b23bb6132fa8d8
Luciano Danilo <pelu.lucho@gmail.com>
2025-01-27 17:56:02 -0300

    Se agrego en el frontend el traer las notificaciones desde la api

--- frontend/src/componentes/MenuNotificaciones.jsx
+++ frontend/src/componentes/MenuNotificaciones.jsx
@@ -1,34 +1,47 @@
 import { BellIcon } from "@chakra-ui/icons";
 import { Badge, Button, IconButton } from "@chakra-ui/react";
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 
 const MenuNotificaciones = () => {
     const [notificationCount, setNotificacionCount] = useState([]);
 
+    const traerNotificaciones = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/api/notificaciones');
+        setNotificacionCount(response.data.data.length);
+      } catch (error) {
+        console.error("No se pudo traer las notificaciones", error);
+      }
+    }
+
     useEffect(() => {
-        setNotificacionCount(4);
+        traerNotificaciones();
     },[])
     return (<>
         <Button
-            variant="outline"
-            bg="white"
-            _hover={{ bg: "pink.200" }}
-            size="md"
-            >
-            <BellIcon />
-            {notificationCount > 0 && (
-                  <Badge
-                    colorScheme="red"
-                    borderRadius="full"
-                    fontSize="0.8em"
-                    ml={-2}
-                    mt={-3}
-                  >
-                    {notificationCount}
-                  </Badge>
-                )}
-        </Button>
+          variant="outline"
+          bg="white"
+          _hover={{ bg: "pink.200" }}
+          size="md"
+          maxW={50}
+          position="relative"  // Hace que el Badge esté posicionado sobre el Button
+      >
+          <BellIcon />
+          {notificationCount > 0 && (
+              <Badge
+                  colorScheme="red"
+                  borderRadius="full"
+                  fontSize="0.8em"
+                  position="absolute" // Lo hace flotar sobre el icono
+                  top={0.5}  // Ajusta la posición vertical
+                  right={2}  // Ajusta la posición horizontal
+              >
+                  {notificationCount}
+              </Badge>
+          )}
+      </Button>
     </>
 )}
 
